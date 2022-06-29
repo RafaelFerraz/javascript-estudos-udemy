85e90a60af457dd0173f6d82c8985136074a49f9
Rafael Ferraz dos Santos <rafael.ferraz@live.com>
2022-06-29 01:07:38 -0300

    Seção 2 - Tipos de Dados e Operadores Lógicos
    Adicionados exemplos da seção 2, que trata de tipos de dados e operadores lógicos em JavaScript.

---
+++ 2_tipos_de_dados_e_operadores_logicos/4_string/scripts.js
@@ -0,0 +1,5 @@
+console.log(typeof ("Oi, meu nome é Rafael"));
+console.log(typeof ('Eu queria comprar um piano.'));
+console.log(typeof (`Teste`));
+console.log(typeof ("Infinity"));
+console.log(typeof (Infinity));
