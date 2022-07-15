6ac6db17559f358763f86dafcc2a713cde9000c3
Rafael Ferraz dos Santos <rafael.ferraz@live.com>
2022-07-14 23:51:08 -0300

    Iniciada seção 4
    Iniciada Seção 4 - Estruturas de Programação

---
+++ 4_estruturas_de_programacao/1_variaveis/scripts.js
@@ -0,0 +1,24 @@
+//Definição de uma variável
+let nome = "Rafael";
+console.log(nome);
+
+//Mesclar string com variáveis usando template literals
+console.log(`O meu nome é ${nome}!`);
+
+//Operações com variáveis
+let laranjas = 5;
+console.log(laranjas * laranjas);
+
+//Reatribuição de valores em variáveis já declaradas
+nome = "João";
+console.log(nome);
+laranjas = 854123;
+console.log(laranjas);
+
+//Alteração de tipo de dados em variáveis já declaradas
+laranjas = "Laranja";
+console.log(laranjas);
+
+//Declaração de múltiplas variáveis
+let um = 1, dois = 2, tres = 3;
+console.log(um + dois + tres);
---
+++ 4_estruturas_de_programacao/2_variaveis_const_var/scripts.js
@@ -0,0 +1,6 @@
+// Declaração de variáveis usando var e const
+var nome = "Rafael";
+const ip = "127.0.0.1";
+
+console.log(nome);
+console.log(ip);
