98d82fa9882814f9df30afca88890ecfaead0ed7
Rafael Ferraz dos Santos <rafael.ferraz@live.com>
2022-08-25 01:31:10 -0300

    Funções built-in do Javascript

---
+++ 4_estruturas_de_programacao/7_math/scripts.js
@@ -0,0 +1,15 @@
+let maior = Math.max(6, 12, 28, 55, 8);
+
+console.log(maior);
+
+let menor = Math.min(6, 12, 28, 55, 8);
+
+console.log(menor);
+
+let arredondar = Math.round(5.28547521);
+
+console.log(arredondar);
+
+let arredondarParaCima = Math.ceil(5.28547521);
+
+console.log(arredondarParaCima);
---
+++ 4_estruturas_de_programacao/8_consolelog/scripts.js
@@ -0,0 +1,8 @@
+let idade = 34;
+
+let nome = "Rafael";
+
+console.log(idade);
+console.log(nome);
+
+console.log(`O meu nome é ${nome}, e tenho ${idade} anos.`);
